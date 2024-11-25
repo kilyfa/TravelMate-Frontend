87eb953852b1ff2d51d2846e26f5779ccbb148cb
dixxyy <saputradicky481@gmail.com>
2024-11-25 16:42:49 +0700

    benerin tombol search

--- app/src/main/java/com/example/travelmate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/travelmate/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.travelmate.R
 import com.example.travelmate.databinding.FragmentHomeBinding
 
@@ -22,7 +23,7 @@ class HomeFragment : Fragment() {
 
         // Menangani klik tombol pencarian
         binding.searchButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_filterFragment)
+            findNavController().navigate(R.id.searchButton)
         }
 
         return binding.root
