5d518fd2d4721886a91b25d22b214b814923412d
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-11-28 13:35:29 +0700

    init homefragment for rreccomendation place

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.android.material:material:1.0.0")
 }
