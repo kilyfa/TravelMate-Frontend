2855ba946b0148632a0106cef6b26ac882eb2e34
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-11-28 23:06:48 +0700

    Update HomeFragment.kt

--- app/src/main/java/com/example/travelmate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/travelmate/ui/home/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.travelmate.R
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,7 +22,8 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -84,6 +83,8 @@ class HomeFragment : Fragment() {
         })
     }
 
-        return binding.root
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
